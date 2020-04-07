d370a2f9e4e0ffefb418bed63714580d374fb0d7
arnab <arnab@rudderlabs.com>
2020-04-07 19:23:40 +0530

    Updated core SDK version

--- facebook/src/main/java/com/rudderlabs/android/integration/facebook/FacebookIntegrationFactory.java
+++ facebook/src/main/java/com/rudderlabs/android/integration/facebook/FacebookIntegrationFactory.java
@@ -1,25 +1,26 @@
 package com.rudderlabs.android.integration.facebook;
 
-
 import android.os.Bundle;
 
 import com.facebook.FacebookSdk;
 import com.facebook.appevents.AppEventsLogger;
-import com.rudderlabs.android.sdk.core.RudderClient;
-import com.rudderlabs.android.sdk.core.RudderIntegration;
-import com.rudderlabs.android.sdk.core.RudderMessage;
+import com.rudderstack.android.sdk.core.MessageType;
+import com.rudderstack.android.sdk.core.RudderClient;
+import com.rudderstack.android.sdk.core.RudderConfig;
+import com.rudderstack.android.sdk.core.RudderIntegration;
+import com.rudderstack.android.sdk.core.RudderLogger;
+import com.rudderstack.android.sdk.core.RudderMessage;
+import com.rudderstack.android.sdk.core.RudderTraits;
 
-import java.util.HashMap;
 import java.util.Map;
 
 public class FacebookIntegrationFactory extends RudderIntegration<AppEventsLogger> {
-    static final String FACEBOOK_KEY = "FB";
-    static final String FACEBOOK_DISPLAY_NAME = "Facebook";
-    private static final String FACEBOOK_TYPE = "type";
+    private static final String FACEBOOK_KEY = "Facebook App Events";
     private AppEventsLogger instance;
+
     public static Factory FACTORY = new Factory() {
         @Override
-        public RudderIntegration<?> create(Object settings, RudderClient client) {
+        public RudderIntegration<?> create(Object settings, RudderClient client, RudderConfig rudderConfig) {
             return new FacebookIntegrationFactory(settings, client);
         }
 
@@ -29,68 +30,83 @@ public class FacebookIntegrationFactory extends RudderIntegration<AppEventsLogge
         }
     };
 
-    private Map<String, String> eventMap = new HashMap<>();
-
     private FacebookIntegrationFactory(Object config, RudderClient client) {
-        String facebookApplicationId = (String) ((Map<String, Object>) config).get("apiKey");
-        FacebookSdk.setApplicationId(facebookApplicationId);
-        FacebookSdk.sdkInitialize(client.getApplication());
-        FacebookSdk.setAutoInitEnabled(true);
-        FacebookSdk.setAutoLogAppEventsEnabled(true);
-        FacebookSdk.fullyInitialize();
-        AppEventsLogger.activateApp(client.getApplication(), facebookApplicationId);
+        if (config != null && client != null && client.getApplication() != null) {
+            String facebookApplicationId = (String) ((Map<String, Object>) config).get("apiKey");
+            FacebookSdk.setApplicationId(facebookApplicationId);
+            FacebookSdk.sdkInitialize(client.getApplication());
+            FacebookSdk.setAutoInitEnabled(true);
+            FacebookSdk.setAutoLogAppEventsEnabled(true);
+            FacebookSdk.fullyInitialize();
+            AppEventsLogger.activateApp(client.getApplication(), facebookApplicationId);
 
-        this.instance = AppEventsLogger.newLogger(client.getApplication());
+            this.instance = AppEventsLogger.newLogger(client.getApplication());
+        } else {
+            RudderLogger.logError("Facebook Factory is not initialized");
+        }
     }
 
     private void processRudderEvent(RudderMessage element) {
-        Bundle paramBundle = Utils.getBundleForMap(element.getProperties());
-        Bundle userBundle = Utils.getBundleForMap(element.getUserProperties());
+        String messageType = element.getType();
+        if (messageType != null && instance != null) {
+            switch (messageType) {
+                case MessageType.IDENTIFY:
+                    AppEventsLogger.setUserID(element.getUserId());
+                    RudderTraits.Address address = RudderTraits.Address.fromString(
+                            RudderTraits.getAddress(element.getTraits())
+                    );
+                    AppEventsLogger.setUserData(
+                            // email
+                            RudderTraits.getEmail(element.getTraits()),
+                            // firstName
+                            RudderTraits.getFirstname(element.getTraits()),
+                            // lastName
+                            RudderTraits.getLastname(element.getTraits()),
+                            // phone
+                            RudderTraits.getPhone(element.getTraits()),
+                            // dateOfBirth
+                            RudderTraits.getBirthday(element.getTraits()),
+                            // gender
+                            RudderTraits.getGender(element.getTraits()),
+                            // city
+                            address != null ? address.getCity() : null,
+                            // state
+                            address != null ? address.getState() : null,
+                            // zip
+                            address != null ? address.getPostalCode() : null,
+                            // country
+                            address != null ? address.getCountry() : null
+                    );
+                    break;
+                case MessageType.TRACK:
+                    Bundle paramBundle = Utils.getBundleForMap(element.getProperties());
+                    if (paramBundle == null) {
+                        instance.logEvent(element.getEventName());
+                    } else {
+                        instance.logEvent(element.getEventName(), paramBundle);
+                    }
+                    break;
+                case MessageType.SCREEN:
 
-        if (paramBundle != null && userBundle != null) paramBundle.putAll(userBundle);
-
-        if (paramBundle == null) instance.logEvent(element.getEventName());
-        else instance.logEvent(element.getEventName(), paramBundle);
-    }
-
-    @Override
-    public void track(RudderMessage track) {
-        processRudderEvent(track);
-    }
-
-    @Override
-    public void identify(RudderMessage identify) {
-        processRudderEvent(identify);
-    }
-
-    @Override
-    public void group(RudderMessage group) {
-        processRudderEvent(group);
-    }
-
-    @Override
-    public void alias(RudderMessage alias) {
-        processRudderEvent(alias);
-    }
-
-    @Override
-    public void screen(RudderMessage screen) {
-        processRudderEvent(screen);
-    }
-
-    @Override
-    public void flush() {
-        super.flush();
+                    break;
+                default:
+                    RudderLogger.logWarn("MessageType is not supported");
+                    break;
+            }
+        }
     }
 
     @Override
     public void reset() {
-        super.reset();
+        AppEventsLogger.clearUserID();
+        AppEventsLogger.clearUserData();
     }
 
     @Override
     public void dump(RudderMessage element) {
-        processRudderEvent(element);
+        if (element != null) {
+            processRudderEvent(element);
+        }
     }
 
     @Override
